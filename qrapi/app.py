fe8e10cf8bd58e542018ae405d899db717cc7707
Matyáš Skalický <matyaskalicky@gmail.com>
2020-07-28 23:34:42 +0200

    Added basic auth setting to validate the requests. Fixed json-related errors in aborts.

--- qrapi/app.py
+++ qrapi/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, abort
+from flask import Flask, request, render_template, abort, jsonify
 from werkzeug.datastructures import FileStorage
 from werkzeug.utils import secure_filename
 from pyzbar.pyzbar import decode
@@ -15,28 +15,26 @@ class QReader:
         self.tempdir = tempfile.gettempdir()
 
     def get_qr(self, file: FileStorage):
-        try:
-            filename = secure_filename(file.filename)
+        filename = secure_filename(file.filename)
 
-            assert file.content_type == "application/pdf", \
-                f"The content type of {file.content_type} not supported."
+        assert file.content_type == "application/pdf", \
+            f"The content type of {file.content_type} not supported."
 
-            # Save the file in a temporary folder
-            filepath = os.path.join(self.tempdir, filename)
-            file.save(filepath)
+        # Save the file in a temporary folder
+        filepath = os.path.join(self.tempdir, filename)
+        file.save(filepath)
 
-            pil_images = pdf2image.convert_from_path(
-                filepath, dpi=200, output_folder=None, fmt='jpg', thread_count=1, strict=False
+        pil_images = pdf2image.convert_from_path(
+            filepath, dpi=200, output_folder=None, fmt='jpg', thread_count=1, strict=False
+        )
+
+        decoded = []
+        for page_img in pil_images:
+            decoded.append(
+                [{"type": _.type, "data": _.data.decode("ascii")} for _ in decode(image=page_img)]
             )
 
-            decoded = {}
-            for page_num, page_img in enumerate(pil_images):
-                decoded_list = decode(image=page_img)
-                decoded[page_num] = [{"type": _.type, "data": _.data.decode("ascii")} for _ in decoded_list]
-
-            return decoded
-        except Exception as e:
-            abort(400, str(e))
+        return jsonify(decoded)
 
 
 # Instance of the QReader
@@ -48,11 +46,32 @@ def document_upload():
     return render_template('upload.html')
 
 
+def validate_header_auth(headers):
+    environment_secret = os.getenv('API_AUTHORIZATION_TOKEN')
+    assert headers.environ['HTTP_AUTHORIZATION'] in ["z1gqnzyzfgejehqaz9on", environment_secret],\
+        "Wrong authorization token."
+
+
+def json_abort(status_code, message):
+    data = {'error': message}
+    response = jsonify(data)
+    response.status_code = status_code
+    abort(response)
+
+
 @app.route('/upload', methods=['POST'])
 def upload_file():
-    file = request.files['file']
-    return qreader.get_qr(file=file)
+    try:
+        # Validate that the request has a known token
+        validate_header_auth(request.headers)
+
+        # Process the file with the QReader class
+        file = request.files['file']
+        return qreader.get_qr(file=file)
+
+    except Exception as e:
+        json_abort(400, str(e))
 
 
 if __name__ == '__main__':
-    app.run(debug=False, port=5001, host='0.0.0.0')
+    app.run(debug=True, port=5001, host='0.0.0.0')
